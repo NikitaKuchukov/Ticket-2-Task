dc24d0d3809d605288d0678dc4345c2c0c538cba
NikitaKuchukov <Kuchukovnik@mail.ru>
2023-01-24 18:56:08 +0700

    task done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "Муза, ранясь шилом опыта, ты помолишься на разум";
+        String strWithoutSpaces = str.replace(" ", "");
+        String strWithoutSpacesAndComma = strWithoutSpaces.replace(",", "");
+        StringBuilder sb = new StringBuilder(strWithoutSpacesAndComma);
+        sb.reverse();
+        if (strWithoutSpacesAndComma.equalsIgnoreCase(sb.toString())) {
+            System.out.println("Строка - палиндром");
+        } else {
+            System.out.println("Строка - не палиндром");
+        }
     }
 }
